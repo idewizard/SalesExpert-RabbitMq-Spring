de1eb334308c7dd6c2e9aa308712fce57270d9de
Andiie <andre.sousa.tec@gmail.com>
2021-09-21 22:07:12 -0300

    Initial commit

---
+++ src/main/java/com/example/SalesExpertRabbitMqSpring/SalesExpertRabbitMqSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.SalesExpertRabbitMqSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SalesExpertRabbitMqSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SalesExpertRabbitMqSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SalesExpertRabbitMqSpring/SalesExpertRabbitMqSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SalesExpertRabbitMqSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SalesExpertRabbitMqSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
